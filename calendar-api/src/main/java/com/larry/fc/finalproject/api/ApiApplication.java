f2fe1b965fad7e5218b9aad73e860f11eeb4fa82
larryjung <charryhub@gmail.com>
2021-07-24 23:47:46 +0900

    feat: 디팬던시 설정, 심플 엔티티 코드 추가

---
+++ calendar-api/src/main/java/com/larry/fc/finalproject/api/ApiApplication.java
@@ -0,0 +1,40 @@
+package com.larry.fc.finalproject.api;
+
+import com.larry.fc.finalproject.core.SimpleEntity;
+import com.larry.fc.finalproject.core.SimpleEntityRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@EntityScan("com.larry.fc.finalproject.core")
+@EnableJpaRepositories("com.larry.fc.finalproject.core")
+@RestController
+@SpringBootApplication
+public class ApiApplication {
+
+    private final SimpleEntityRepository repository;
+
+    public ApiApplication(SimpleEntityRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<SimpleEntity> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/save")
+    public SimpleEntity saveOne() {
+        return repository.save(new SimpleEntity());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
