daf60afe3f0a9cec5276086a1a1496945ade5542
larry.charry <larry.charry@kakaopaycorp.com>
2021-08-01 22:45:36 +0900

    feat: argument Resolver AuthUser 추가 순서 1. argument resolver 추가하면서 소스 설명, 실제 구현체들 탐색 2. 적용 및 테스트

---
+++ calendar-api/src/main/java/com/larry/fc/finalproject/api/config/AuthUserResolver.java
@@ -0,0 +1,33 @@
+package com.larry.fc.finalproject.api.config;
+
+import com.larry.fc.finalproject.api.dto.AuthUser;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static com.larry.fc.finalproject.api.service.LoginService.LOGIN_SESSION_KEY;
+
+/**
+ * @author Larry
+ */
+public class AuthUserResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return AuthUser.class.isAssignableFrom(parameter.getParameterType());
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        final Long userId = (Long) webRequest.getAttribute(LOGIN_SESSION_KEY,
+                WebRequest.SCOPE_SESSION);
+        if (userId != null) {
+            return AuthUser.of(userId);
+        } else {
+            throw new RuntimeException("bad request. no session");
+        }
+    }
+}
---
+++ calendar-api/src/main/java/com/larry/fc/finalproject/api/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.larry.fc.finalproject.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+
+/**
+ * @author Larry
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthUserResolver());
+    }
+}
