2fa175e48e5a1858793f7a152f4da3d73e22e48c
larry.charry <larry.charry@kakaopaycorp.com>
2021-08-02 19:42:55 +0900

    feat: 주,월 조회 로직 개발 리팩토링 2 순서 1. 함수로 추출 (오바한듯)

--- calendar-api/src/main/java/com/larry/fc/finalproject/api/service/ScheduleQueryService.java
+++ calendar-api/src/main/java/com/larry/fc/finalproject/api/service/ScheduleQueryService.java
@@ -7,12 +7,15 @@ import com.larry.fc.finalproject.core.domain.entity.repository.EngagementReposit
 import com.larry.fc.finalproject.core.domain.entity.repository.ScheduleRepository;
 import com.larry.fc.finalproject.core.util.Period;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -42,7 +45,7 @@ public class ScheduleQueryService {
         return getSchedulesByPeriod(authUser, period);
     }
 
-    public List<ForListScheduleDto> getSchedulesByPeriod(AuthUser authUser, Period period) {
+    private List<ForListScheduleDto> getSchedulesByPeriod(AuthUser authUser, Period period) {
         return Stream.concat(
                 scheduleRepository
                         .findAllByWriter_Id(authUser.getId())
@@ -55,5 +58,35 @@ public class ScheduleQueryService {
                         .filter(engagement -> engagement.isOverlapped(period))
                         .map(engagement -> DtoConverter.toForListDto(engagement.getSchedule()))
         ).collect(toList());
+    }
+
+    private List<ForListScheduleDto> getSchedulesByPeriod2(AuthUser authUser, Period period) {
+        return Stream.concat(
+                getSchedulesByFunction(
+                        () -> scheduleRepository.findAllByWriter_Id(authUser.getId()).stream()
+                                .map(s -> Pair.of(s::isOverlapped, s)),
+                        period,
+                        (s) -> DtoConverter.toForListDto(s)
+                ),
+                getSchedulesByFunction(
+                        () -> engagementRepository
+                                .findAllByAttendeeId(authUser.getId()).stream()
+                                .map(e -> Pair.of(e::isOverlapped, e)),
+                        period,
+                        (e) -> DtoConverter.toForListDto(e.getSchedule())
+                )
+        ).collect(toList());
+    }
+
+    private <T> Stream<ForListScheduleDto> getSchedulesByFunction(Supplier<Stream<Pair<OverlapCheckable, T>>> supplier,
+                                                                  Period period,
+                                                                  Function<T, ForListScheduleDto> func) {
+        return supplier.get()
+                .filter(pair -> pair.getFirst().isOverlapped(period))
+                .map(pair -> func.apply(pair.getSecond()));
+    }
+
+    interface OverlapCheckable {
+        boolean isOverlapped(Period period);
     }
 }
